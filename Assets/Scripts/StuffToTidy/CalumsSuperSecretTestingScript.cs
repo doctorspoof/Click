91c8cbe73c3a65edd8f42c0a1ef0bb48d651ba50
doctorspoof <calum.haigh@r8games.com>
2015-04-15 14:03:05 +0100

    Merged Sunny's version of project
    Merged the two current versions of the project into one. Should mean the
    repository is now the central, correct, version.

--- Assets/Scripts/StuffToTidy/CalumsSuperSecretTestingScript.cs
+++ Assets/Scripts/StuffToTidy/CalumsSuperSecretTestingScript.cs
@@ -6,10 +6,12 @@ public class CalumsSuperSecretTestingScript : MonoBehaviour {
 	//[SerializeField]	Shader	ShaderToRenderWith;
 	//Camera CachedCameraComponent;
 	[SerializeField]	GameObject	Rectangle;
+	public Material Shite; 
 	
 	// Use this for initialization
 	void Start () 
 	{
+		Shite = Rectangle.GetComponent<Renderer>().material;
 	}
 	
 	// Update is called once per frame
@@ -17,6 +19,16 @@ public class CalumsSuperSecretTestingScript : MonoBehaviour {
 	{
 		//CachedCameraComponent.SetReplacementShader(ShaderToRenderWith, "ForwardBase");
 		Texture RenderTex = GetComponent<Camera> ().targetTexture;
+		GetComponent<Camera>().depthTextureMode = DepthTextureMode.DepthNormals;
 		Rectangle.GetComponent<Renderer>().material.SetTexture("_MainTex", RenderTex);
+		
+		if(Input.GetKey(KeyCode.Z))
+		{
+			Shite.SetFloat("_Intensity", Shite.GetFloat("_Intensity") + 0.01f);
+		}
+		if(Input.GetKey(KeyCode.X))
+		{
+			Shite.SetFloat("_Intensity", Shite.GetFloat("_Intensity") - 0.01f);
+		}
 	}
 }
---
+++ Assets/Scripts/StuffToTidy/Click_Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click_Test : MonoBehaviour
+{
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+			GetComponent<Click_Clicker>().Toggle();
+	}
+	
+	void OnGUI()
+	{
+		//GUI.Label(new Rect(0, 0, 200, 50), "CLICK TO TOGGLE FX");
+	}
+}
