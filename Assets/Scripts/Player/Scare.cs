41c1954fd35043ce01cb9e946fc90554971781b9
SunjayD <sunjay.dayalji@r8games.com>
2015-04-16 14:01:49 +0100

    Motion Blur
    Motion Blur added to camera but currently disabled

---
+++ Assets/Scripts/Player/Scare.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scare : MonoBehaviour {
+
+	Scare_Manager sManager;
+
+	void Start()
+	{
+		sManager = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Scare_Manager> ();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter () {
+	
+	}
+}
