a70651cfd0c1613d2dbaef15dbf7a856c59a1974
doctorspoof <calum.haigh@r8games.com>
2015-04-20 18:04:49 +0100

    Added camera effects and fear system
    Essentially merged Sunny's build onto master

--- Assets/Scripts/Player/Scare.cs
+++ Assets/Scripts/Player/Scare.cs
@@ -4,14 +4,30 @@ using System.Collections;
 public class Scare : MonoBehaviour {
 
 	Scare_Manager sManager;
+	float scarePercent = 10;
 
 	void Start()
 	{
 		sManager = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Scare_Manager> ();
 	}
-	
-	// Update is called once per frame
-	void OnTriggerEnter () {
-	
+
+	void Update()
+	{
+		RaycastHit hit;
+		Vector3 fwd= transform.TransformDirection(Vector3.forward);
+
+		if (Physics.Raycast(transform.position, fwd, out hit)) 
+		{
+			if(hit.distance <= 10.0f && hit.collider.gameObject.tag == "Enemy")
+			{
+				sManager.currentScarePercentage += scarePercent * Time.deltaTime;
+			}
+
+			if(hit.distance > 10.0f)
+			{
+				sManager.currentScarePercentage -= scarePercent * Time.deltaTime;
+			}
+		}
 	}
+
 }
