1ff074b24c1f6fe674df9d77f553f4200058b302
TomTrz <trzpil.tomasz@gmail.com>
2015-04-20 09:34:15 +0100

    input and patrolling
    Added new input for the controller and new patrolling script

--- Assets/Scripts/Enemies/Patrolling.cs
+++ Assets/Scripts/Enemies/Patrolling.cs
@@ -2,55 +2,78 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Patrolling : MonoBehaviour {
+public class Patrolling : MonoBehaviour
+{
 	
-	List<Vector3> roamingPoints;
-	GameObject[] roamingPointsRef;
+	List<Vector3> PatrolPoints;
+	Vector3[] localPatrolPoints;
 	bool newTargetSet;
 	
 	int target;
+
+	public int PatrollingPointsID;
+	public int initialPatrollingPointsID;
 	
 	void Start()
 	{
-		roamingPoints = new List<Vector3>();
-		
-		roamingPointsRef = GameObject.FindGameObjectsWithTag("RoamingPoint");
-		
-		for(int i = 0; i < roamingPointsRef.Length; i++)
-		{
-			roamingPoints.Add(roamingPointsRef[i].transform.position);
-		}
-		
+		PatrolPoints = GameObject.FindGameObjectWithTag("PatrolPointManager").GetComponent<PatrolPointManager>().RequestPatrolPoints(PatrollingPointsID);
+
+		print(PatrolPoints.Count);
 		newTargetSet = false;
 		target = 0;
+
+		initialPatrollingPointsID = PatrollingPointsID;
 	}
 
 	void Update()
 	{
-	
+		//print(PatrolPoints.Count);
+	}
+
+	public List<Vector3> GetPatrolRoute()
+	{
+		if(PatrollingPointsID == -1)
+		{
+			List<Vector3> localPoints = new List<Vector3>();
+			for(int i = 0; i < transform.childCount; i++)
+			{
+				if(transform.GetChild(i).tag == "PatrolPoint")
+				{
+					//chachehek this
+					localPoints.Add(new Vector3(transform.GetChild(i).position.x, transform.GetChild(i).position.y, transform.GetChild(i).position.z));
+				}
+			}
+			return localPoints;
+		}
+		else
+		{
+			return PatrolPoints;
+		}
 	}
 	
 	public Vector3 Patrol(NavMeshAgent navAgent)
 	{
-		if(HasReachedTarget(roamingPoints[target]))
+		if(PatrolPoints.Count > 0)
 		{
-			if(!newTargetSet)
+			if(HasReachedTarget(GetPatrolRoute()[target]))
 			{
-				StartCoroutine(WaitBeforeNextTarget());
-				newTargetSet = true;
+				if(!newTargetSet)
+				{
+					StartCoroutine(WaitBeforeNextTarget());
+					newTargetSet = true;
+				}
 			}
+			else
+			{
+				newTargetSet = false;
+			}
+			//print (target);
+			//print (HasReachedTarget(roamingPoints[target]));
+			
+			return GetPatrolRoute()[target];
 		}
-		else
-		{
-			newTargetSet = false;
-		}
-		
-		
-		
-		//print (target);
-		//print (HasReachedTarget(roamingPoints[target]));
-		
-		return roamingPoints[target];
+		//Don't roam when there is no roaming points
+		return transform.position;
 	}
 	
 	bool HasReachedTarget(Vector3 currentTarget)
@@ -62,7 +85,7 @@ public class Patrolling : MonoBehaviour {
 		else
 		{
 			return false;
-		}	
+		}
 	}
 	
 	IEnumerator WaitBeforeNextTarget()
@@ -74,10 +97,9 @@ public class Patrolling : MonoBehaviour {
 			timer += Time.deltaTime;
 			yield return 0;
 		}
-		print (target);
 		target++;
 		
-		if(target >= roamingPointsRef.Length)
+		if(target >= PatrolPoints.Count)
 		{
 			target = 0;
 		}
